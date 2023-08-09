be98af336bd71a925324b608803048fa5e72db32
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-08-10 01:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,11 +31,13 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
+function rot13(str) {
+  let decodedArr = []; 
+	for (let letter of str) {
+    if(letter>='A' && letter<='Z')decodedArr.push(lookup[letter]);
+    else decodedArr.push(letter);
+}
+  return decodedArr;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
